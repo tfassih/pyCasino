4c5dee33f0f05a36051c42bb9711319d4ba74c2a
tfassih <thomasfassih@gmail.com>
2025-02-22 12:58:43 -0600

    tired

--- game_client.py
+++ game_client.py
@@ -66,20 +66,6 @@ def start_game():
                                                                        'transform: translate(-50%, -50%);'
                                                                        )
 
-class Pairs_Card_Toggle(ui.button):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self._state = False
-        self.on('click', self.toggle)
-
-    def toggle(self) -> None:
-        self._state = not self._state
-        self.update()
-
-    def update(self) -> None:
-        self.props(f'icon={"/assets/img/back@2x.png" if self._state else "card_image"}')
-        super().update()
-
 
 @ui.page('/options_menu')
 def options_menu():
@@ -90,33 +76,61 @@ def options_menu():
 def blackjack():
     ui.query('body').style(f'background-color: #000000; min-height: 1200px;')
     ui.label('Blackjack - Coming Soon').style('color: #FFFFFF;')
-
+card_pair = []
+matches = 0
+counter = [0]
 @ui.page('/pairs')
 def pairs():
-
+    global matches
+    global card_pair
     card_image = "/assets/img/back@2x.png"
-
+    images = []
     ui.query('body').style(f'background-color: #000000; min-height: 1200px;')
     ui.label('Pairs - Coming Soon').style('color: #FFFFFF;')
+    matches_label = ui.label(f'Matches: {matches}').style('color: #FFFFFF;')
     pairs_game = Pairs()
     selected_cards = pairs_game.selected_cards
     print(selected_cards)
     random.shuffle(selected_cards)
-
+    global counter
     print("SLC: " + str(selected_cards))
 
-    def switch_image(c_button, image, c):
-        if image == "/assets/img/back@2x.png":
-            c_button.set_source(c[2])
-            print(c[2])
-        else:
-            c_button.set_source("/assets/img/back@2x.png")
-        ui.update(c_button)
+    def switch_image(card_button, card_img, c):
+        global counter
+        global card_pair
+        if counter[0] < 3:
+            new_img = c[2] if card_img == "/assets/img/back@2x.png" else "/assets/img/back@2x.png"
+            card_button.set_source(new_img)
+
+            counter[0] += 1
+            card_pair.append(c)
+            if counter[0] > 1 and len(card_pair) > 1:
+                print("check")
+                check_pair_vals(card_pair[0], card_pair[1], matches_label)
+                card_pair = []
+            if counter[0] == 3:
+                for img_btn in images:
+                    img_btn.set_source("/assets/img/back@2x.png")
+                card_pair = []
+                counter[0] = 0
+
     with ui.grid(columns=3, rows=4).style(f'height: 25%; width: 25%; column-gap: 10px; row-gap: 10px; ').classes('col-gap-10, row-gap-10;'):
         for card in selected_cards:
             with ui.button().classes('row-span-1, h-[272px] ;'):
                 img = ui.image(card_image)
+                images.append(img)
                 img.on('click', lambda ui_img=img, image=card_image, c=card: switch_image(ui_img, image, c))
+                ui.update(img)
+
+def check_pair_vals(card1, card2, ml):
+    global matches
+    print(card1)
+    print(card2)
+    if card1 == card2:
+        matches += 1
+        ml.set_text(f'Matches: {matches}')
+        print("MATCHES: " + str(matches))
+
 
 
 
